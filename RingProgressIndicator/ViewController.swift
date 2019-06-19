08804c214865e34ab6680ecdf0b0773bfd9d69fa
Sudhanshu <sudhanshu@timesinternet.in>
2019-06-19 14:52:54 +0530

    ring updated

--- RingProgressIndicator/ViewController.swift
+++ RingProgressIndicator/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     let badgeImageView: BadgeView = {
-        let imageView = BadgeView(image: UIImage(named: "mypic"))
+        let imageView = BadgeView(image: UIImage(named: "mypic"), fromValue: 0)
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
@@ -22,15 +22,29 @@ class ViewController: UIViewController {
         
         self.view.addSubview(badgeImageView)
         badgeImageView.centerInSuperview()
-        badgeImageView.setupRing()
-        print(badgeImageView)
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.badgeImageView.update(value: 0.25, animated: true)
+        }
     }
 }
 
 class BadgeView: UIImageView, RingProgress {
+    
+    var fromValue: CGFloat
+    var toValue: CGFloat
+    
+    
     override init(image: UIImage?) {
+        self.fromValue = 0
+        self.toValue = 0
         super.init(image: image)
+    }
+    
+    convenience init(image: UIImage?, fromValue: CGFloat) {
+        self.init(image: image)
+        self.fromValue = fromValue
+        self.prepare()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -39,21 +53,35 @@ class BadgeView: UIImageView, RingProgress {
 }
 
 
-protocol RingProgress where Self: UIView {}
+protocol RingProgress where Self: UIView {
+    var fromValue: CGFloat{get set}
+    var toValue: CGFloat{get set}
+}
 
 extension RingProgress {
-    func setupRing(animated: Bool = true) {
+    
+    func prepare(_ value: CGFloat = 0) {
+        
         self.backgroundColor = .yellow
         self.clipsToBounds = true
         self.backgroundColor = .clear
         
+        drawShapeLayer(value: value)
+    }
+    
+    func drawShapeLayer(value: CGFloat, animated: Bool = true) {
+        
         let radius = self.bounds.height / 2
         self.layer.cornerRadius = radius
-
+        
         let lineWidth: CGFloat = 4
         
-        let startAngle: CGFloat = -(.pi / 2)
-        let endAngle: CGFloat = .pi//(.pi * 3/2)
+        let offset: CGFloat = (.pi / 2)
+        let startAngle: CGFloat = -offset
+        
+        let defaultFullCircleAngle: CGFloat = (2 * .pi)
+        let endAngle: CGFloat = defaultFullCircleAngle * value - offset
+        
         let circle = UIBezierPath(arcCenter: CGPoint(x: radius, y: radius), radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
         
         let shapeLayer = CAShapeLayer()
@@ -71,12 +99,19 @@ extension RingProgress {
             drawingAnimation.fromValue = 0
             drawingAnimation.toValue = 1
             drawingAnimation.duration = 2
-            drawingAnimation.damping = 9
+            drawingAnimation.damping = 9.5
             drawingAnimation.initialVelocity = 2
             drawingAnimation.timingFunction = CAMediaTimingFunction(name: .linear)
             shapeLayer.add(drawingAnimation, forKey: "drawRing")
         }
+    }
+    
+    func update(value: CGFloat, animated: Bool = true) {
         
+        guard 0...1 ~= value else {
+            fatalError("RingProgress: value=\(value) out of range (0...1)")
+        }
+        drawShapeLayer(value: value, animated: animated)
     }
 }
 
